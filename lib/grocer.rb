6e49756515a89eeb4c5c85f57a39a19769f4e3b6
Kathleen Brennan <brennank14@gmail.com>
2020-06-17 18:34:39 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,7 +9,7 @@ end
 def consolidate_cart(cart)
   new_cart = []
   cart.map do |element|
-    if new_cart.include? element == false
+    if new_cart.include?(element) == false
       new_cart.push(element)
       element.push[:count] => 1
     else
